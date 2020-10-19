9e97c256f9ba279c9d0ea9de5e60cf1d48b92409
jojoohee <67132141+jojoohee@users.noreply.github.com>
2020-10-19 16:02:29 +0900

    Js: Arrow up

--- main.js
+++ main.js
@@ -34,7 +34,20 @@ navbarMenu.addEventListener('click', (event) => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
   });
 
+  // show "arrow-up button"
+  const arrowUp = document.querySelector('.arrow-up');
+  document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight /2) {
+      arrowUp.classList.add('visible');
+    } else {
+      arrowUp.classList.remove('visible');
+    }
+  });
 
+  // Click handler
+  arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home')
+  })
 
   function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
