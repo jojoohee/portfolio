e98dbf308437d216d92be89b4a804dc4c9ec6d3c
jojoohee <67132141+jojoohee@users.noreply.github.com>
2020-10-19 15:40:06 +0900

    Js: transparent

--- main.js
+++ main.js
@@ -27,6 +27,15 @@ navbarMenu.addEventListener('click', (event) => {
     scrollIntoView('#contact');
   });
 
+  // home fade
+  const home = document.querySelector('.home__container');
+  const homeHeight = home.getBoundingClientRect().height;
+  document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+  });
+
+
+
   function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth' });
