ce72ce73f7b78fbc86d6381d75ec5fcbda76a2d7
jojoohee <67132141+jojoohee@users.noreply.github.com>
2020-10-19 15:15:45 +0900

    Js: Home contact button

--- main.js
+++ main.js
@@ -9,3 +9,26 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar__dark');
   }
 });
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return;
+  }
+    scrollIntoView(link);
+  });
+
+  // "contact me" button
+  const homeContactBtn = document.querySelector('.home__contact');
+  homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+  });
+
+  function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth' });
+
+  }
